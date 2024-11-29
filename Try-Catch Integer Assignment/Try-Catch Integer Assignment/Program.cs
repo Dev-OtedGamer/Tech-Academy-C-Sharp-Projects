2d7d467ca73dd131383f7d977f667173c89e8950
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-11-28 21:03:46 -0800

    Added Try-catch Integer Assignment

---
+++ Try-Catch Integer Assignment/Try-Catch Integer Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+
+		List<int> numbers = new List<int> { 10, 15, 20, 25, 30 };
+
+			try
+			{
+				Console.WriteLine("Pick a number.");
+				int numberOne = Convert.ToInt32(Console.ReadLine());
+
+				foreach (int number in numbers)
+				{
+					Console.WriteLine(number + " divided by " + numberOne + " = " + number / numberOne);
+
+				}
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("Please don't divide by zero");
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Please type a whole number");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("An unexpected error occurred: " + ex.Message);
+			}
+			finally
+			{
+				Console.WriteLine("Exited the try/catch block. Program execution continues...");
+			}
+
+		Console.ReadLine();
+	}
+}
