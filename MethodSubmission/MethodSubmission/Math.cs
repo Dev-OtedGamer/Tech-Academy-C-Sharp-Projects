1f7ca45c1a5c06eb25ba06c489ced9958da78dad
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-02 19:26:57 -0800

    Added MethodSubmission

---
+++ MethodSubmission/MethodSubmission/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+	public class Math
+	{
+		//Fist method math operation -1
+		public int SubtractOne(int number)
+		{
+			return number - 1;
+		}
+		
+		//Second method math operation +15
+		public int Addfifteen(int number)
+		{ 
+			return number + 15; 
+		}
+
+		//Third method math operation x3
+		public int MutiplyXThree(int number)
+		{
+			return number * 3;
+		}
+
+
+	}
+}
---
+++ MethodSubmission/MethodSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmission
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				//
+				Math math = new Math();
+
+				Console.WriteLine("Please enter a number we can do some math operations on.");
+				int userInput = Convert.ToInt32(Console.ReadLine());
+
+				Console.WriteLine("\nResults:");
+				Console.WriteLine("Subtracting 1 from " + userInput + " equals " + math.SubtractOne(userInput));
+				Console.WriteLine("Adding 15 to " + userInput + " equals " + math.Addfifteen(userInput));
+				Console.WriteLine("Multiplying " + userInput + " by 3 equals " + math.MutiplyXThree(userInput));
+			}
+			catch (FormatException ex)
+			{
+				Console.WriteLine("Please type a whole number.");
+				return;
+			}
+
+		}
+	}
+}
