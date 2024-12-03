3ac8601298d88f9fa0f4b09b9fb3cab0e45be008
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-03 01:45:26 -0800

    Added voidMethodSubmission

---
+++ voidMethodSubmission/voidMethodSubmission/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethodSubmission
+{
+	public class Math
+	{
+		public void plusTen(int num1, int num2)
+		{
+			int result = num1 + 10;
+
+			Console.WriteLine($"The first number plus 10 equals {result}");
+			Console.WriteLine($"The second number is {num2}");
+		}
+	}
+}
---
+++ voidMethodSubmission/voidMethodSubmission/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidMethodSubmission
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Math math = new Math();
+
+			math.plusTen(7, 11);
+			
+			math.plusTen(num1: 1, num2: 2);
+
+			Console.ReadLine();
+		}
+	}
+}
