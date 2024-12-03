a7cec821b7e8493a83190cb7c8e021591b80fb40
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-02 22:48:54 -0800

    Added mainMethodSubmission

---
+++ mainMethodSubmission/mainMethodSubmission/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mainMethodSubmission
+{
+	public class Math
+	{
+		public int Add25(int i)
+		{
+			return i + 25;
+		}
+
+		public int Add25(decimal i)
+		{
+			return (int)(i * 2);
+		}
+
+		public int Add25(string i)
+		{
+			if (int.TryParse(i, out int result))
+			{
+				return result - 1;
+			}
+			else
+			{
+				throw new ArgumentException("The Provided String is not a valid integer.");
+			}
+
+		}
+	}
+}
