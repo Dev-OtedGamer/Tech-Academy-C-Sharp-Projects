6f3c58b0b7a041b04cfa98650c92b1ec7615f22f
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-06 10:31:08 -0800

    Added Void Divison assignment

---
+++ voidDivisionSubmission/voidDivisionSubmission/Math.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidDivisionSubmission
+{
+	public class Math
+	{
+		public void divide2(int num1)
+		{
+			int result = num1 / 2;
+
+			Console.WriteLine($"{num1} divided by 2 equals {result}");
+		}
+
+		public void PramOutputs(int num2, out int add10, out int sub1)
+		{
+			add10 = num2 + 10;
+			sub1 = num2 - 1;
+		}
+
+		public int PramOutputs(int num1, int num2, int num3)
+		{
+			return num1 * num2 / num3;
+		}
+		public int PramOutputs(int num1, int num2, int num3, int num4)
+		{
+			return num2 * num3 / num1 + num4;
+		}
+
+		public static int add15(int num1)
+		{
+			return num1 + 15;
+		}
+	}
+}
---
+++ voidDivisionSubmission/voidDivisionSubmission/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voidDivisionSubmission
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Math math = new Math();
+			
+			Console.WriteLine("Please enter a number.");
+			int userInput = Convert.ToInt32(Console.ReadLine());
+			math.divide2(userInput);
+
+			Console.WriteLine("Please enter another number.");
+			int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+			int addResult;
+			int subResult;
+
+			math.PramOutputs(userInput2, out addResult, out subResult);
+
+			Console.WriteLine($"Adding 10 to {userInput2} equals {addResult}");
+			Console.WriteLine($"Subtracting 1 from {userInput2} equals {subResult}");
+
+			int sum1 = math.PramOutputs(10, 8, 3);
+			Console.WriteLine($"10 * 8 / 3 = {sum1}");
+			int sum2 = math.PramOutputs(10, 8, 3, 0);
+			Console.WriteLine($"8 * 3 / 10 + 0 = {sum2}");
+
+			int sum3 = Math.add15(3);
+			Console.WriteLine($"Adding 15 to 3 = {sum3}");
+
+			Console.ReadLine();
+		}
+	}
+}
