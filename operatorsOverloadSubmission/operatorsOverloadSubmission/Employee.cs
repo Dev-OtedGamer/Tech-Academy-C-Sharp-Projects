26b7dd4a4e2bdd815d7bc0468ead6784a1d66e04
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-08 22:19:12 -0800

    Added operatorOverloadSubmission

---
+++ operatorsOverloadSubmission/operatorsOverloadSubmission/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorsOverloadSubmission
+{
+	public class Employee
+	{
+        public int EmpID { get; set; }
+
+		public static bool operator ==(Employee emp1, Employee emp2)
+		{
+			return emp1.EmpID == emp2.EmpID;
+		}
+
+		public static bool operator !=(Employee emp1, Employee emp2)
+		{ 
+			return !(emp1 == emp2); 
+		}
+
+    }
+}
---
+++ operatorsOverloadSubmission/operatorsOverloadSubmission/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorsOverloadSubmission
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Employee employee1 = new Employee { EmpID = 1 };
+			Employee employee2 = new Employee { EmpID = 2 };
+			Employee employee3 = new Employee { EmpID = 1 };
+
+			Console.WriteLine($"Comparing employee ID's ");
+			Console.WriteLine("Is employee1 equal to employee2? " + (employee1 == employee2));
+			Console.WriteLine("Is employee1 equal to employee3? " + (employee1 == employee3));
+			Console.WriteLine("Is Employee1 not equal to employee2? " + (employee1 != employee2));
+
+		}
+	}
+}
