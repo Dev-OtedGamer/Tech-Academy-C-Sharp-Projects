30de98c7b0506ba5cc3d06e60a6be3e18fada391
Joshua Rollins <Rollinsjoshua77@gmail.com>
2024-12-08 20:00:05 -0800

    Added polymorphismSubmission

---
+++ polymorphismSubmission/polymorphismSubmission/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphismSubmission
+{
+	public class Employee : IQuittable
+	{
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public void Quit()
+		{
+			Console.WriteLine($"{firstName} {lastName} has quit their job.");
+		}
+
+	}
+}
---
+++ polymorphismSubmission/polymorphismSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using polymorphismSubmission;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismSubbmision
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Employee employee = new Employee();
+			{
+				employee.firstName = "Sample";
+				employee.lastName = "Student";
+			}
+
+			IQuittable quittable = employee;
+
+			quittable.Quit();
+
+			Console.ReadLine();
+
+		}
+	}
+}
